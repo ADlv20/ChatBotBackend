dd86fffab564f153541d6fc49790f4d1d7041f3c
ADwait <84146707+ADlv20@users.noreply.github.com>
2022-05-23 22:12:16 +0530

    Flask Module Complete give message in url

--- app.py
+++ app.py
@@ -1,17 +1,94 @@
-from crypt import methods
-from flask import Flask, jsonify, request
+import nltk
+from flask import Flask, jsonify
+
+nltk.download('all')
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+import pickle
+
+import numpy as np
+# from tensorflow.keras.optimizers import SGD
+
+from keras.models import load_model
+
+# Path to h5 model generated
+model = load_model('ML/chatbot_model.h5')
+import json
+import random
+
+# Path to Intent file downloaded from Kaggle
+intents = json.loads(open('ML/Intent.json').read())
+
+# Path to Words.pkl file
+words = pickle.load(open('ML/words.pkl', 'rb'))
+
+# Path to classes.pkl
+classes = pickle.load(open('ML/classes.pkl', 'rb'))
+
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print("found in bag: %s" % w)
+    return (np.array(bag))
+
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if (i['intent'] == tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET", "POST"])
-def home():
-    data = {
-        "Modules": 15,
-        "Subject": "Data Structures and Algorithms",
-    }
-    return jsonify(data)
+@app.route('/<string:msg>', methods=["GET", "POST"])
+def home(msg):
+    res = chatbot_response(msg)
+    print(res)
+    return jsonify(response=res)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
